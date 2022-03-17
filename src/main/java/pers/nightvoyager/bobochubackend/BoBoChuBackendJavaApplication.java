e73e32530c80847364b7cd096acc6bd46428a848
Night-Voyager <18722007@bjtu.edu.cn>
2022-03-17 09:33:28 +0800

    feat: initial commit

---
+++ src/main/java/pers/nightvoyager/bobochubackend/BoBoChuBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package pers.nightvoyager.bobochubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoBoChuBackendJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoBoChuBackendJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pers/nightvoyager/bobochubackend/BoBoChuBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.nightvoyager.bobochubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoBoChuBackendJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
