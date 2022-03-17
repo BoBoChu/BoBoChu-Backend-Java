fbe3e9654a95ad523ae756819ba1fc49e899c898
Night-Voyager <18722007@bjtu.edu.cn>
2022-03-17 20:58:47 +0800

    feat: add framework of service for websocket

---
+++ src/main/java/pers/nightvoyager/bobochubackend/service/WebSocketService.java
@@ -0,0 +1,34 @@
+package pers.nightvoyager.bobochubackend.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+
+@Component
+@Service
+@ServerEndpoint("/api/websocket")
+public class WebSocketService {
+    private static final GameService gameService = new GameService();
+
+    @OnOpen
+    public void onOpen(Session session) {
+
+    }
+
+    @OnClose
+    public void onClose() {
+
+    }
+
+    @OnMessage
+    public void onMessage(Session session, String message) {
+
+    }
+
+    @OnError
+    public void onError(Session session, Throwable e) {
+        e.printStackTrace();
+    }
+}
